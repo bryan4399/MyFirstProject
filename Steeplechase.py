e27a4d9b2b8c08989bdd9cca45e4a94d5ff7b66f
bryan4399 <104889016+bryan4399@users.noreply.github.com>
2022-05-04 15:15:19 +0800

    Add files via upload

---
+++ Steeplechase.py
@@ -0,0 +1,77 @@
+"""
+File: Steeplechase.py
+Name: TODO:
+---------------------------------
+TODO:
+"""
+
+from karel.stanfordkarel import *
+
+
+def main():
+    """
+    Karel crosses hurdles in a 12x12 world
+    with a for loop 
+    """
+    for i in range(11):
+        if front_is_clear():
+            move()
+        else:
+            jump()
+
+
+def jump():
+    up()
+    cross()
+    down()
+
+
+def up():
+    while not front_is_clear():
+        turn_left()
+        while not right_is_clear():
+            move()
+
+
+def cross():
+    turn_right()
+    move()
+    turn_right()
+
+
+def turn_right():
+    for i in range(3):
+        turn_left()
+
+def down():
+    while front_is_clear():
+        move()
+    turn_left()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####### DO NOT EDIT CODE BELOW THIS LINE ########
+
+if __name__ == '__main__':
+    execute_karel_task(main)
